c6b8026f89f5ea53c060ed2deb86086daf87a8b9
zihadulislam2 <zihadul708@gmail.com>
2025-08-28 05:05:01 +0600

    added index

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import userRoutes from './routes/user.routes'
 import { globalErrorHandler } from './middlewares/globalErrorHandler'
 import { notFound } from './middlewares/notFound'
 const app = express()
+import router from './routes'
+
 
 app.use(express.json())
 
-app.use('/api/users', userRoutes)
+app.use('/api/v1', router)
 
 app.use(notFound as never)
 app.use(globalErrorHandler)
